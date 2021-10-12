53d888738b4613b32ed2175e0a3733c0ca33c7e4
Nirweint <nirweint_1@mail.ru>
2021-10-12 12:10:39 +0300

    add 3 types of todolist's

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,23 @@ function App() {
         {id: 2, title: "CSS", isDone: true},
         {id: 3, title: "React", isDone: false},
     ];
+    const task_2: Array<TaskType> = [
+        {id: 1, title: "Bread", isDone: true},
+        {id: 2, title: "tomato", isDone: false},
+        {id: 3, title: "potatoes", isDone: true},
+    ];
+    const task_3: Array<TaskType> = [
+        {id: 1, title: "HTML book", isDone: true},
+        {id: 2, title: "CSS book", isDone: true},
+        {id: 3, title: "React site", isDone: false},
+    ];
+
 
     return (
         <div className="App">
             <TodoList title={"What to learn"} tasks={task_1}/>
-            {/*<TodoList title={"What to buy"}/>*/}
-            {/*<TodoList title={"What to read"}/>*/}
+            <TodoList title={"What to buy"} tasks={task_2}/>
+            <TodoList title={"What to read"} tasks={task_3}/>
         </div>
     );
 }

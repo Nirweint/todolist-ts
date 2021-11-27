4320b419d94d1726706bcfe0cc9c8c0d0d23ebe6
Nirweint <nirweint_1@mail.ru>
2021-11-27 17:15:57 +0300

    Updates

---
+++ precache-manifest.6a1d8d2f176206244deeee4037919e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e238a906e89e5d709302e6a1a5552fe1",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "4385a6d4b7f511290439",
+    "url": "/todolist-ts/static/css/main.df810a07.chunk.css"
+  },
+  {
+    "revision": "8eee78d900f721fc5434",
+    "url": "/todolist-ts/static/js/2.8f9511a5.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist-ts/static/js/2.8f9511a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4385a6d4b7f511290439",
+    "url": "/todolist-ts/static/js/main.fc3781e4.chunk.js"
+  },
+  {
+    "revision": "b7c7c0b009241d8831c6",
+    "url": "/todolist-ts/static/js/runtime-main.8e0caec2.js"
+  }
+]);
